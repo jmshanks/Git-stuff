88bd4c07c29398a7c624812e96343c6a98a3eff6
jmshanks <46826324+jmshanks@users.noreply.github.com>
2019-03-07 01:03:57 -0600

    Create Bicycle.java

---
+++ Java1/Transportation/Bicycle.java
@@ -0,0 +1,42 @@
+@@ -0,0 +1,41 @@
+package com.company;
+
+abstract class Bicycle extends Transport {
+
+    int numberOfTires;
+    int numberOfGears;
+
+
+    Bicycle(Transport Bicycle) {
+
+        super();
+        numberOfTires = 4;
+
+
+    }
+
+    public Bicycle() {
+        super();
+
+        numberOfTires = 4;
+        numberOfGears = 2;
+    }
+
+    void printStats() {
+
+        System.out.println("Bicycle: \n" +
+                "Tires: " + this.numberOfTires + "\n" +
+                "Gears: " + this.numberOfGears + "\n" +
+                "Max Speed: " + this.maxSpeed() + " mph." + "\n" +
+                "Time in ¼ Mile: " + time() + " seconds." + "\n");
+
+
+    }
+
+    public double time() {
+        double distance = 1320;
+        return distance / maxSpeed();
+    }
+
+
+}
