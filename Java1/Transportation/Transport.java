c30a94b5aacc7f831cbc310d48d2b333786c1a02
jmshanks <46826324+jmshanks@users.noreply.github.com>
2019-03-07 01:03:15 -0600

    Create Transport.java

---
+++ Java1/Transportation/Transport.java
@@ -0,0 +1,13 @@
+@@ -0,0 +1,12 @@
+package com.company;
+
+public abstract class Transport {
+
+    public abstract double time();
+
+    public abstract double maxSpeed();
+
+    public void distance(int i) {
+    }
+
+}
