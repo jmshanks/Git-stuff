fb97da49f2ec1e8c874aef3fee5134477368d4bf
jmshanks <46826324+jmshanks@users.noreply.github.com>
2019-03-12 20:18:36 -0500

    Create Person.java

---
+++ Java1/Final/Person.java
@@ -0,0 +1,14 @@
+package javaonefinal.people;
+
+public class Person {
+
+    static String name;
+    static int age;
+
+    public Person (String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+}
