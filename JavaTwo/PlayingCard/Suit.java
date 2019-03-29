39f6e8ba061e49cb0c21410c182062a0688bea6f
jmshanks <46826324+jmshanks@users.noreply.github.com>
2019-03-28 22:23:46 -0500

    Create Suit.java

---
+++ JavaTwo/PlayingCard/Suit.java
@@ -0,0 +1,20 @@
+package javatwo;
+
+public enum Suit {
+
+    CLUBS(1),
+    DIAMONDS(2),
+    HEARTS(3),
+    SPADES(4);
+
+    private final int suitValue;
+
+    Suit(final int suitValue) {
+        this.suitValue = suitValue;
+    }
+
+    public int getSuitValue() {
+        return this.suitValue;
+    }
+}
+
